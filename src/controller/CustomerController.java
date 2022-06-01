d9c2532bee35edf1a6bf72c79485dfc0cec3f33f
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-06-01 23:48:05 +0700

    updated customer controller, table shown user data and search feature is usable now

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.User;
+import tools.AlertTools;
 import tools.BackBtnTools;
+import tools.DatabaseTools;
+import tools.ValidationTools;
 
 public class CustomerController {
 
@@ -23,6 +32,33 @@ public class CustomerController {
 
     @FXML
     private TableColumn<User, String> usernameCol;
+
+    @FXML
+    private TextField searchTf;
+
+    public void initialize() {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        usernameCol.setCellValueFactory(new PropertyValueFactory<>("username"));
+        phoneNumberCol.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        blacklistedCol.setCellValueFactory(new PropertyValueFactory<>("blacklisted"));
+
+        try {
+            Connection connection = DatabaseTools.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users WHERE role = 'user'");
+
+            while (resultSet.next()) {
+                table.getItems()
+                        .add(new User(resultSet.getInt("id"), resultSet.getString("username"),
+                                resultSet.getString("password"), resultSet.getString("role"),
+                                resultSet.getString("phone_number"), resultSet.getString("created_at")));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @FXML
     void addCustomerBtn(ActionEvent event) {
@@ -46,12 +82,31 @@ public class CustomerController {
 
     @FXML
     void searchBtn(ActionEvent event) {
+        if (ValidationTools.isTextFieldEmptyOrNull(this.searchTf)) {
+            AlertTools.AlertError("Error!", "Search text field is empty!", "Please enter a valid input!");
+            return;
+        }
 
-    }
+        String searchText = this.searchTf.getText();
 
-    @FXML
-    void searchTf(ActionEvent event) {
+        try {
+            Connection connection = DatabaseTools.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users WHERE role = 'user' AND username LIKE '%"
+                    + searchText + "%'");
 
+            table.getItems().clear();
+
+            while (resultSet.next()) {
+                table.getItems()
+                        .add(new User(resultSet.getInt("id"), resultSet.getString("username"),
+                                resultSet.getString("password"), resultSet.getString("role"),
+                                resultSet.getString("phone_number"), resultSet.getString("created_at")));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
