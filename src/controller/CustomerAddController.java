0a2e2f2cd3d232b0ee0d9eb6b64fe27bdefdc134
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-06-02 21:32:11 +0700

    customer add completed

--- src/controller/CustomerAddController.java
+++ src/controller/CustomerAddController.java
@@ -1,9 +1,15 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import model.User;
+import tools.AlertTools;
 import tools.BackBtnTools;
+import tools.DatabaseTools;
+import tools.ValidationTools;
 
 public class CustomerAddController {
 
@@ -17,17 +23,94 @@ public class CustomerAddController {
     private TextField usernameTf;
 
     public void initialize() {
-
+        defaultTf();
     }
 
     @FXML
     void addBtn(ActionEvent event) {
+
+        if (ValidationTools.isTextFieldEmptyOrNull(usernameTf, passwordTf, phoneNumberTf)) {
+            AlertTools.AlertError("Error!", "Text field consist of blank!", "Please fill in all text field!");
+
+            defaultTf();
+
+            return;
+        }
+
+        String username = usernameTf.getText();
+        String password = passwordTf.getText();
+        String phoneNumber = phoneNumberTf.getText();
+
+        if (!ValidationTools.isPhoneNumberValid(phoneNumber)) {
+            AlertTools.AlertError("Error!", "Invalid phone number!", "Please enter a valid phone number!");
+
+            defaultTf();
+
+            return;
+        }
+
+        if (password.length() < 8) {
+            AlertTools.AlertError("Error!", "Password is not valid!", "Please enter a valid password!");
+
+            defaultTf();
+
+            return;
+        }
+
+        if (User.isValueExist("phone_number", phoneNumber)) {
+            AlertTools.AlertError("Error!", "Phone number is already exist!", "Please enter a valid phone number!");
+
+            defaultTf();
+
+            return;
+        }
+
+        if (User.isValueExist("username", username)) {
+            AlertTools.AlertError("Error!", "Username is already exist!", "Please enter a valid username!");
+
+            defaultTf();
+
+            return;
+        }
+
+        String sql = "INSERT INTO users (username, password, role, phone_number, created_at) VALUES (?, ?, 'user', ?, NOW())";
+
+        try {
+            Connection connection = DatabaseTools.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, phoneNumber);
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) {
+                AlertTools.AlertInformation("Success!", "User has been added!", "User has been added!");
+                BackBtnTools.backBtnActionEvent(event);
+            } else {
+                AlertTools.AlertInformation("Error!", "User has not been added!", "User has not been added!");
+
+                defaultTf();
+            }
+
+            DatabaseTools.closeQueryOperation(connection, preparedStatement);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
     void backBtn(ActionEvent event) {
         BackBtnTools.backBtnActionEvent(event);
+    }
+
+    private void defaultTf() {
+        usernameTf.setText("");
+        passwordTf.setText("");
+        phoneNumberTf.setText("+628");
     }
 
 }
