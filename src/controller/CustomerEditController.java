c4851036f56b7116fdbbba69794ca1ffa0b47886
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-06-02 21:33:42 +0700

    customer edit controller has completely done

--- src/controller/CustomerEditController.java
+++ src/controller/CustomerEditController.java
@@ -3,7 +3,10 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import model.User;
+import tools.AlertTools;
 import tools.BackBtnTools;
+import tools.ValidationTools;
 
 public class CustomerEditController {
 
@@ -16,6 +19,8 @@ public class CustomerEditController {
     @FXML
     private TextField usernameTf;
 
+    private User user;
+
     @FXML
     void backBtn(ActionEvent event) {
         BackBtnTools.backBtnActionEvent(event);
@@ -23,7 +28,59 @@ public class CustomerEditController {
 
     @FXML
     void editBtn(ActionEvent event) {
+        if (ValidationTools.isTextFieldEmptyOrNull(usernameTf, passwordTf, phoneNumberTf)) {
+            AlertTools.AlertError("Error!", "Text field consist of blank!", "Please fill in all text field!");
 
+            defaultTf();
+
+            return;
+        }
+
+        String password = passwordTf.getText();
+
+        if (password.equals(user.getPassword())) {
+            AlertTools.AlertError("Error!", "Password is not change!", null);
+
+            defaultTf();
+
+            return;
+        }
+
+        if (password.length() < 8) {
+            AlertTools.AlertError("Error!", "Password is not valid!", "Please enter a valid password!");
+
+            defaultTf();
+
+            return;
+        }
+
+        if (user.changePassword(password)) {
+            AlertTools.AlertConfirmation("Success!", "Password is changed!", null);
+
+            BackBtnTools.backBtnActionEvent(event);
+        } else {
+            AlertTools.AlertError("Error!", "Password is not changed!", null);
+
+            defaultTf();
+        }
+
+        System.out.println(user.getPassword());
+
+    }
+
+    void setUser(User user) {
+        this.user = user;
+
+        defaultTf();
+    }
+
+    private void defaultTf() {
+        usernameTf.setText(user.getUsername());
+        passwordTf.setText(user.getPassword());
+        phoneNumberTf.setText(user.getPhoneNumber());
+
+        usernameTf.setDisable(true);
+        phoneNumberTf.setDisable(true);
     }
 
 }
