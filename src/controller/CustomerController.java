f456516f20c2cdd59b0e60d3e2db93e0d7c14c26
Ziancarlos <86158278+Ziancarlos@users.noreply.github.com>
2022-06-02 21:32:33 +0700

    customer controller has completely done

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,16 +8,21 @@ import java.sql.Statement;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.User;
 import tools.AlertTools;
 import tools.BackBtnTools;
 import tools.DatabaseTools;
 import tools.SwitchSceneTools;
 import tools.ValidationTools;
+import javafx.scene.Node;
 
 public class CustomerController {
 
@@ -64,25 +70,6 @@ public class CustomerController {
     }
 
     @FXML
-    void addCustomerBtn(ActionEvent event) {
-        SwitchSceneTools.changeSceneActionEvent(event, "../view/customer-add-page.fxml");
-
-        BackBtnTools.addToBackBtnStack("../view/customers-page.fxml");
-    }
-
-    @FXML
-    void detailCustomerBtn(ActionEvent event) {
-
-    }
-
-    @FXML
-    void editCustomerBtn(ActionEvent event) {
-        SwitchSceneTools.changeSceneActionEvent(event, "../view/customer-edit-page.fxml");
-
-        BackBtnTools.addToBackBtnStack("../view/customers-page.fxml");
-    }
-
-    @FXML
     void searchBtn(ActionEvent event) {
         if (ValidationTools.isTextFieldEmptyOrNull(this.searchTf)) {
             AlertTools.AlertError("Error!", "Search text field is empty!", "Please enter a valid input!");
@@ -117,6 +104,81 @@ public class CustomerController {
     @FXML
     void backBtn(ActionEvent event) {
         BackBtnTools.backBtnActionEvent(event);
+    }
+
+    @FXML
+    void addCustomerBtn(ActionEvent event) {
+        SwitchSceneTools.changeSceneActionEvent(event, "../view/customer-add-page.fxml");
+
+        BackBtnTools.addToBackBtnStack("../view/customers-page.fxml");
+    }
+
+    @FXML
+    void detailCustomerBtn(ActionEvent event) {
+        User user = table.getSelectionModel().getSelectedItem();
+
+        if (user != null) {
+            try {
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/customer-details-page.fxml"));
+
+                Parent root = loader.load();
+
+                CustomerDetailController controller = loader.getController();
+
+                controller.setUser(user);
+
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                Scene scene = new Scene(root);
+
+                stage.setScene(scene);
+
+                stage.show();
+
+                BackBtnTools.addToBackBtnStack("../view/customers-page.fxml");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            AlertTools.AlertConfirmation("Error", "No User Selected!", "Please select valid user!");
+        }
+    }
+
+    @FXML
+    void editCustomerBtn(ActionEvent event) {
+
+        User user = table.getSelectionModel().getSelectedItem();
+
+        if (user != null) {
+            try {
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/customer-edit-page.fxml"));
+
+                Parent root = loader.load();
+
+                CustomerEditController controller = loader.getController();
+
+                controller.setUser(user);
+
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                Scene scene = new Scene(root);
+
+                stage.setScene(scene);
+
+                stage.show();
+
+                BackBtnTools.addToBackBtnStack("../view/customers-page.fxml");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            AlertTools.AlertConfirmation("Error", "No User Selected!", "Please select valid user!");
+        }
     }
 
 }
