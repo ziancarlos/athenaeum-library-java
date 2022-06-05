cd8859b495d73b37ec8d8224d4477ea56c6170d6
zian <ziancrlswong@gmail.com>
2022-06-06 01:05:13 +0700

    on prog menu controller

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -12,13 +12,11 @@ public class MenuController {
 
     @FXML
     void logoutBtn(ActionEvent event) {
-
         if (AlertTools.AlertConfirmation("Logout confirmation", "Are you sure? ", null).get() == ButtonType.OK) {
             SwitchSceneTools.changeSceneActionEvent(event, "../view/login-page.fxml");
 
             CurrentUser.currentUser = null;
         }
-
     }
 
     @FXML
@@ -32,5 +30,37 @@ public class MenuController {
 
         BackBtnTools.addToBackBtnStack("../view/menu-page.fxml");
     }
+
+    @FXML
+    void booksBtn(ActionEvent event) {
+        SwitchSceneTools.changeSceneActionEvent(event, "../view/books-page.fxml");
+
+        BackBtnTools.addToBackBtnStack("../view/menu-page.fxml");
+    }
+
+    @FXML
+    void purchasingBtn(ActionEvent event) {
+        if (CurrentUser.currentUser.getRole().equals("user")) {
+            AlertTools.AlertError("Error!", "You dont have permission to access this page!", null);
+            return;
+        }
+
+        SwitchSceneTools.changeSceneActionEvent(event, "../view/purchasings-page.fxml");
+
+        BackBtnTools.addToBackBtnStack("../view/menu-page.fxml");
+    }
+
+    @FXML
+    void categoryBtn(ActionEvent event) {
+        if (CurrentUser.currentUser.getRole().equals("user")) {
+            AlertTools.AlertError("Error!", "You dont have permission to access this page!", null);
+            return;
+        }
+
+        SwitchSceneTools.changeSceneActionEvent(event, "../view/categories-page.fxml");
+
+        BackBtnTools.addToBackBtnStack("../view/menu-page.fxml");
+    }
+
 
 }
