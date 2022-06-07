729bff4d34db1debe2d34ae98a0bb4a3db82df8f
zian <ziancrlswong@gmail.com>
2022-06-08 01:48:57 +0700

    purchase add controller completed

--- src/controller/PurchaseAddController.java
+++ src/controller/PurchaseAddController.java
@@ -9,6 +9,7 @@ import java.util.LinkedList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -20,6 +21,7 @@ import model.Category;
 import tools.AlertTools;
 import tools.BackBtnTools;
 import tools.DatabaseTools;
+import tools.UiTools;
 import tools.ValidationTools;
 
 public class PurchaseAddController {
@@ -55,7 +57,7 @@ public class PurchaseAddController {
 
     private double totalAmount = 0;
 
-    public void initialize(){
+    public void initialize() {
         setCategoryCb();
 
         bookNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -65,38 +67,46 @@ public class PurchaseAddController {
 
     @FXML
     void addBtn(ActionEvent event) {
-        if(ValidationTools.isTextFieldEmptyOrNull(bookNameTf, priceTf)){
+        if (ValidationTools.isTextFieldEmptyOrNull(bookNameTf, priceTf)) {
             AlertTools.AlertError("Error!", "Text Field Is Empty!", "Please Dont Leave A Blank!");
 
-            defaultAddBookTf();
+            setDefaultTfAddBook();
 
             return;
         }
 
-        if(!ValidationTools.isDouble(priceTf.getText())){
+        if (!ValidationTools.isDouble(priceTf.getText())) {
             AlertTools.AlertError("Error!", "Price Is Not A Number!", "Please Enter A Valid Number!");
 
-            defaultAddBookTf();
+            setDefaultTfAddBook();
 
             return;
         }
 
-
-        if(categoryCb.getSelectionModel().getSelectedItem() == null){
+        if (categoryCb.getSelectionModel().getSelectedItem() == null) {
             AlertTools.AlertError("Error!", "Category Is Not Selected!", "Please Select A Category!");
 
-            defaultAddBookTf();
-            
+            setDefaultTfAddBook();
+
             return;
         }
 
-        books.add(new Book(bookNameTf.getText(), categoryCb.getSelectionModel().getSelectedItem(), Double.parseDouble(priceTf.getText())));
+        if (Double.parseDouble(priceTf.getText()) <= 0) {
+            AlertTools.AlertError("Error!", "Price Is Not Valid!", "Please Enter A Valid Price!");
 
-        totalAmountRefresh(Double.parseDouble(priceTf.getText()));
+            setDefaultTfAddBook();
+
+            return;
+        }
+
+        books.add(new Book(bookNameTf.getText(), categoryCb.getSelectionModel().getSelectedItem(),
+                Double.parseDouble(priceTf.getText())));
+
+        addTotalAmount(Double.parseDouble(priceTf.getText()));
 
         refreshTable();
 
-        defaultAddBookTf();
+        setDefaultTfAddBook();
     }
 
     @FXML
@@ -106,11 +116,13 @@ public class PurchaseAddController {
 
     @FXML
     void deleteBtn(ActionEvent event) {
-        if(table.getSelectionModel().getSelectedItem() == null){
+        if (table.getSelectionModel().getSelectedItem() == null) {
             AlertTools.AlertError("Error!", "Book Is Not Selected!", "Please Select A Book!");
 
             return;
         }
+
+        deleteTotalAmount(table.getSelectionModel().getSelectedItem().getBoughtPrice());
 
         books.remove(table.getSelectionModel().getSelectedItem());
 
@@ -119,15 +131,24 @@ public class PurchaseAddController {
 
     @FXML
     void purchaseBtn(ActionEvent event) {
-        if(books.size() == 0){
+        if (books.size() == 0) {
             AlertTools.AlertError("Error!", "No Book Is Added!", "Please Add A Book!");
-    
+
+            everythingDefault();
+
             return;
         }
 
-        if(ValidationTools.isTextFieldEmptyOrNull(supplierNameTf)){
-            AlertTools.AlertError("Error!", "Supplier Name Text Field Is Empty!", "Please Fill Supplier Name Text Field!");
+        if (ValidationTools.isTextFieldEmptyOrNull(supplierNameTf)) {
+            AlertTools.AlertError("Error!", "Supplier Name Text Field Is Empty!",
+                    "Please Fill Supplier Name Text Field!");
 
+            return;
+        }
+
+        if (AlertTools.AlertConfirmation("Are You Sure This Detail You Given Is Correct ? ",
+                "Input You Make Cant Be Edited!", "Please Make Sure Detail You Given Is Correct!")
+                .get() == ButtonType.CANCEL) {
             return;
         }
 
@@ -136,41 +157,47 @@ public class PurchaseAddController {
         PreparedStatement preparedStatementBooks = null;
         ResultSet resultSet = null;
 
-        try{
+        try {
             connection = DatabaseTools.getConnection();
             connection.setAutoCommit(false);
 
-            preparedStatementPurchasing = connection.prepareStatement("INSERT INTO purchasings(supplier_name, date, amount) VALUES(?, NOW(), ?)", Statement.RETURN_GENERATED_KEYS);
+            preparedStatementPurchasing = connection.prepareStatement(
+                    "INSERT INTO purchasings(supplier_name, date, amount) VALUES(?, NOW(), ?)",
+                    Statement.RETURN_GENERATED_KEYS);
             preparedStatementPurchasing.setString(1, supplierNameTf.getText());
             preparedStatementPurchasing.setDouble(2, totalAmount);
 
             int affectedRows = preparedStatementPurchasing.executeUpdate();
 
-            if(affectedRows > 0){
+            if (affectedRows > 0) {
                 resultSet = preparedStatementPurchasing.getGeneratedKeys();
 
-                if(resultSet.next()){
+                if (resultSet.next()) {
                     int purchasingId = resultSet.getInt(1);
 
-                    preparedStatementBooks = connection.prepareStatement("INSERT INTO books(name, availability, category_id, bought_price, purchasing_id) VALUES(?, ?, ?, ?, ?)");
-                    
+                    preparedStatementBooks = connection.prepareStatement(
+                            "INSERT INTO books(name, availability, category_id, bought_price, purchasing_id) VALUES(?, ?, ?, ?, ?)");
+
                     for (Book book : books) {
                         preparedStatementBooks.setString(1, book.getName());
                         preparedStatementBooks.setInt(2, 1);
                         preparedStatementBooks.setInt(3, book.getCategory().getId());
                         preparedStatementBooks.setDouble(4, book.getBoughtPrice());
                         preparedStatementBooks.setInt(5, purchasingId);
-    
+
                         preparedStatementBooks.executeUpdate();
                     }
-                    
+
                     connection.commit();
 
-                    AlertTools.AlertInformation("Success!", "Purchase Is Successfully Added!", "Purchase Is Successfully Added!");
+                    AlertTools.AlertInformation("Success!", "Purchase Is Successfully Added!",
+                            "Purchase Is Successfully Added!");
 
                     everythingDefault();
+
+                    BackBtnTools.backBtnActionEvent(event);
                 }
-            }else{
+            } else {
                 AlertTools.AlertError("Error!", "Purchasing Is Not Added!", "Please Try Again!");
 
                 connection.rollback();
@@ -179,7 +206,7 @@ public class PurchaseAddController {
 
                 return;
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             try {
                 connection.rollback();
@@ -188,63 +215,71 @@ public class PurchaseAddController {
             }
 
             everythingDefault();
-        }finally {
+        } finally {
             try {
-                if(resultSet != null)  resultSet.close();
-                if(preparedStatementBooks != null) preparedStatementBooks.close();
-                if(preparedStatementPurchasing != null) preparedStatementPurchasing.close();
-                if(connection != null) connection.close();
-                
+                if (resultSet != null)
+                    resultSet.close();
+                if (preparedStatementBooks != null)
+                    preparedStatementBooks.close();
+                if (preparedStatementPurchasing != null)
+                    preparedStatementPurchasing.close();
+                if (connection != null)
+                    connection.close();
+
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
         }
-        
+
     }
 
     private void setCategoryCb() {
-        try{
+        try {
             Connection connection = DatabaseTools.getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM categories");
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 categoryCb.getItems().add(new Category(resultSet.getInt("id"), resultSet.getString("name")));
             }
 
             DatabaseTools.closeQueryOperation(connection, statement, resultSet);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void refreshTable(){
+    private void refreshTable() {
         table.getItems().clear();
         table.getItems().addAll(books);
     }
 
-    private void defaultAddBookTf(){
-        bookNameTf.setText("");
-        priceTf.setText("");
+    private void setDefaultTfAddBook() {
+        UiTools.setTextFieldEmpty(bookNameTf, priceTf);
         categoryCb.getSelectionModel().clearSelection();
     }
 
-    private void totalAmountRefresh(double totalAmount){
-        this.totalAmount += totalAmount;
-
-        totalAmountText.setText("$ " + String.valueOf(this.totalAmount));
-    }
-
-    private void everythingDefault(){
+    private void everythingDefault() {
         this.totalAmount = 0;
         totalAmountText.setText("$ " + String.valueOf(totalAmount));
 
         books.clear();
+
         refreshTable();
 
-        defaultAddBookTf();
+        setDefaultTfAddBook();
 
-        supplierNameTf.setText("");
+        UiTools.setTextFieldEmpty(supplierNameTf);
+    }
+
+    private void addTotalAmount(double amount) {
+        this.totalAmount += amount;
+        totalAmountText.setText("$ " + String.valueOf(totalAmount));
+    }
+
+    private void deleteTotalAmount(double amount) {
+        this.totalAmount -= amount;
+        totalAmountText.setText("$ " + String.valueOf(totalAmount));
     }
 
 }
