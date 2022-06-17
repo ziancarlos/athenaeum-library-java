2cad3947d06703665ce9685d96fc45f6700d3a8d
zian <ziancrlswong@gmail.com>
2022-06-18 00:21:42 +0700

    completed

--- src/controller/CategoryEditController.java
+++ src/controller/CategoryEditController.java
@@ -1,11 +1,16 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import model.Category;
 import tools.AlertTools;
-import tools.BackBtnTools;
+import tools.BackBtn;
+import tools.DatabaseTools;
 import tools.ValidationTools;
 
 public class CategoryEditController {
@@ -16,15 +21,22 @@ public class CategoryEditController {
     private Category category;
 
     @FXML
-    void backBtn(ActionEvent event) {
-        BackBtnTools.backBtnActionEvent(event);
+    void backOnAction(ActionEvent event) {
+        BackBtn.backBtnActionEvent(event);
     }
 
     @FXML
-    void editBtn(ActionEvent event) {
+    void editOnAction(ActionEvent event) {
         if (ValidationTools.isTextFieldEmptyOrNull(categoryNameTf)) {
-            AlertTools.AlertError("Error!", "Category Name Text Field Is Empty!",
-                    "Please Fill Category Name Text Field!");
+            AlertTools.showAlertError("Text field is empty!", "Please fill in all fields");
+
+            setDefaultTf();
+
+            return;
+        }
+
+        if (!ValidationTools.isTextIsValid(3, 46, categoryNameTf.getText())) {
+            AlertTools.showAlertError("Text is invalid!", "Please check your text");
 
             setDefaultTf();
 
@@ -32,32 +44,90 @@ public class CategoryEditController {
         }
 
         if (category.getName().equals(categoryNameTf.getText())) {
-            AlertTools.AlertError("Error!", "Category Name Is Not Changed", null);
+            AlertTools.showAlertError("Category not changed!", "Please check your category name");
 
             setDefaultTf();
 
             return;
         }
 
-        if (Category.isCategoryNameExist(categoryNameTf.getText(), category.getId())) {
-            AlertTools.AlertError("Error!", "Category Name Is Already Exist!", "Please Enter Another Category Name!");
+        if (Category.isExistWithout(category, categoryNameTf.getText())) {
+            AlertTools.showAlertError("Category already exists!", "Please check your category name");
 
             setDefaultTf();
 
             return;
         }
 
-        if (Category.editCategory(category, categoryNameTf.getText())) {
-            AlertTools.AlertInformation("Success!", "Category Edited Successfully!",
-                    "Category Name: " + categoryNameTf.getText());
+        Connection connection = null;
+        PreparedStatement statementSelect = null;
+        PreparedStatement statementUpdate = null;
+        ResultSet resultSet = null;
+        int affectedRows = 0;
 
-            BackBtnTools.backBtnActionEvent(event);
-        } else {
-            AlertTools.AlertError("Error!", "Category Not Edited!", "Please Try Again!");
+        try {
+            connection = DatabaseTools.getConnection();
+            connection.setAutoCommit(false);
 
-            setDefaultTf();
+            statementSelect = connection.prepareStatement("SELECT * FROM categories WHERE id != ? AND name = ?");
+            statementSelect.setInt(1, category.getId());
+            statementSelect.setString(2, categoryNameTf.getText());
+
+            resultSet = statementSelect.executeQuery();
+            if (!resultSet.next()) {
+                statementUpdate = connection.prepareStatement("UPDATE categories SET name = ? WHERE id = ?");
+                statementUpdate.setString(1, categoryNameTf.getText());
+                statementUpdate.setInt(2, category.getId());
+
+                affectedRows = statementUpdate.executeUpdate();
+                if (affectedRows > 0) {
+                    connection.commit();
+
+                    AlertTools.showAlertInformation("Category updated!", "Category updated successfully");
+
+                    BackBtn.backBtnActionEvent(event);
+                } else {
+                    connection.rollback();
+
+                    AlertTools.showAlertError("Category not updated!", "Category not updated");
+                }
+
+            } else {
+                AlertTools.showAlertError("Category already exists!", "Please check your category name");
+
+                setDefaultTf();
+
+                connection.rollback();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (Exception ex) {
+                    AlertTools.showAlertError("Error!", "Error while updating category");
+                }
+            }
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statementSelect != null) {
+                    statementSelect.close();
+                }
+                if (statementUpdate != null) {
+                    statementUpdate.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                AlertTools.showAlertError("Error!", "Error while updating category");
+            }
         }
-
     }
 
     public void setCategory(Category category) {
@@ -66,7 +136,7 @@ public class CategoryEditController {
         setDefaultTf();
     }
 
-    public void setDefaultTf() {
+    private void setDefaultTf() {
         categoryNameTf.setText(category.getName());
     }
 
