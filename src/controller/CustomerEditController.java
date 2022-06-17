990d389d18f1d339172082ec72f61c0e49f0f6ad
zian <ziancrlswong@gmail.com>
2022-06-18 00:23:28 +0700

    complete

--- src/controller/CustomerEditController.java
+++ src/controller/CustomerEditController.java
@@ -1,11 +1,21 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import model.User;
+import model.Customer;
 import tools.AlertTools;
-import tools.BackBtnTools;
+import tools.BackBtn;
+import tools.DatabaseTools;
+import tools.MD5;
 import tools.ValidationTools;
 
 public class CustomerEditController {
@@ -19,65 +29,165 @@ public class CustomerEditController {
     @FXML
     private TextField usernameTf;
 
-    private User user;
+    private Customer customer;
 
-    @FXML
-    void backBtn(ActionEvent event) {
-        BackBtnTools.backBtnActionEvent(event);
+    public void initialize() {
+        usernameTf.setDisable(true);
+        phoneNumberTf.setDisable(true);
     }
 
     @FXML
-    void editBtn(ActionEvent event) {
-        if (ValidationTools.isTextFieldEmptyOrNull(usernameTf, passwordTf, phoneNumberTf)) {
-            AlertTools.AlertError("Error!", "Text field consist of blank!", "Please fill in all text field!");
-
-            setDefaultTf();
-
-            return;
-        }
-
-        String password = passwordTf.getText();
-
-        if (password.equals(user.getPassword())) {
-            AlertTools.AlertError("Error!", "Password is not change!", null);
-
-            setDefaultTf();
-
-            return;
-        }
-
-        if (password.length() < 8) {
-            AlertTools.AlertError("Error!", "Password is not valid!", "Please enter a valid password!");
-
-            setDefaultTf();
-
-            return;
-        }
-
-        if (user.changePassword(password)) {
-            AlertTools.AlertConfirmation("Success!", "Password is changed!", null);
-
-            BackBtnTools.backBtnActionEvent(event);
-        } else {
-            AlertTools.AlertError("Error!", "Password is not changed!", null);
-
-            setDefaultTf();
-        }
+    void backOnAction(ActionEvent event) {
+        BackBtn.backBtnActionEvent(event);
     }
 
-    void setUser(User user) {
-        this.user = user;
+    @FXML
+    void editOnAction(ActionEvent event) {
+        if (ValidationTools.isTextFieldEmptyOrNull(passwordTf)) {
+            AlertTools.showAlertError("Password and Phone number text field is empty!", "Please fill in all fields");
+
+            setDefaultTf();
+
+            return;
+        }
+
+        if (!ValidationTools.isTextIsValid(8, 45, passwordTf.getText())) {
+            tools.AlertTools.showAlertError("Password is invalid", "Password must be between 8 and 45 characters");
+
+            setDefaultTf();
+
+            return;
+        }
+
+        Connection connection = null;
+        PreparedStatement preparedStatementSelect = null;
+        PreparedStatement preparedStatementUpdate = null;
+        ResultSet resultSet = null;
+        int affectedRows;
+
+        try {
+            connection = DatabaseTools.getConnection();
+            connection.setAutoCommit(false);
+
+            preparedStatementSelect = connection.prepareStatement("SELECT * FROM users WHERE id = ? FOR UPDATE");
+            preparedStatementSelect.setInt(1, customer.getId());
+
+            resultSet = preparedStatementSelect.executeQuery();
+            if (resultSet.next()) {
+
+                if (!resultSet.getString("password").equals(MD5.getMd5(passwordTf.getText()))) {
+
+                    preparedStatementUpdate = connection
+                            .prepareStatement("UPDATE users SET password = ? WHERE id = ?");
+                    preparedStatementUpdate.setString(1, MD5.getMd5(passwordTf.getText()));
+                    preparedStatementUpdate.setInt(2, customer.getId());
+
+                    affectedRows = preparedStatementUpdate.executeUpdate();
+
+                    if (affectedRows > 0) {
+                        AlertTools.showAlertInformation("Success", "Password has been updated");
+
+                        BackBtn.backBtnActionEvent(event);
+
+                        connection.commit();
+                    } else {
+                        AlertTools.showAlertError("Error", "Password has not been updated");
+
+                        setDefaultTf();
+
+                        connection.rollback();
+                    }
+                } else {
+                    AlertTools.showAlertError("Error!", "Password has not been updated");
+
+                    setDefaultTf();
+
+                    connection.rollback();
+                }
+
+            } else {
+                AlertTools.showAlertError("Error!", "Contact Support!");
+
+                setDefaultTf();
+
+                connection.rollback();
+            }
+        } catch (SQLException e) {
+            AlertTools.showAlertError("Error!", "Contact Support!");
+
+            setDefaultTf();
+
+            e.printStackTrace();
+
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                AlertTools.showAlertError("Error!", "Contact Support!");
+
+            }
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (preparedStatementSelect != null) {
+                    preparedStatementSelect.close();
+                }
+                if (preparedStatementUpdate != null) {
+                    preparedStatementUpdate.close();
+                }
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException e) {
+                AlertTools.showAlertError("Error!", "Contact Support!");
+            }
+        }
+
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
 
         setDefaultTf();
     }
 
     private void setDefaultTf() {
-        usernameTf.setText(user.getUsername());
-        passwordTf.setText(user.getPassword());
-        phoneNumberTf.setText(user.getPhoneNumber());
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DatabaseTools.getConnection();
+            preparedStatement = connection.prepareStatement("SELECT * FROM customers WHERE id = ?");
+            preparedStatement.setInt(1, customer.getId());
+            resultSet = preparedStatement.executeQuery();
 
-        usernameTf.setDisable(true);
-        phoneNumberTf.setDisable(true);
+            if (resultSet.next()) {
+                usernameTf.setText(resultSet.getString("username"));
+                passwordTf.setText("");
+                phoneNumberTf.setText(resultSet.getString("phone_number"));
+            }
+
+        } catch (Exception e) {
+            AlertTools.showAlertError("Error", e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+
+            } catch (Exception e) {
+                AlertTools.showAlertError("Error", e.getMessage());
+            }
+        }
+
     }
 
 }
