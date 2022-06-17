0da07ca2619aef914432b9dbc87a8794121625b2
zian <ziancrlswong@gmail.com>
2022-06-18 00:22:45 +0700

    complete

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -4,195 +4,235 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import model.User;
+import model.Customer;
 import tools.AlertTools;
-import tools.BackBtnTools;
+import tools.BackBtn;
 import tools.DatabaseTools;
 import tools.SwitchSceneTools;
 import tools.UiTools;
 import tools.ValidationTools;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
 
 public class CustomerController {
 
     @FXML
-    private TableColumn<User, String> blacklistedCol;
+    private TableColumn<?, ?> createdAtCol;
 
     @FXML
-    private TableColumn<User, Integer> idCol;
+    private TableColumn<?, ?> blacklistedCol;
 
     @FXML
-    private TableColumn<User, String> phoneNumberCol;
+    private TableColumn<?, ?> idCol;
 
     @FXML
-    private TableView<User> table;
-
-    @FXML
-    private TableColumn<User, String> usernameCol;
+    private TableColumn<?, ?> phoneNumberCol;
 
     @FXML
     private TextField searchTf;
 
-    public void initialize() {
+    @FXML
+    private TableView<Customer> table;
 
+    @FXML
+    private TableColumn<?, ?> usernameCol;
+
+    public void initialize() {
         idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
         usernameCol.setCellValueFactory(new PropertyValueFactory<>("username"));
         phoneNumberCol.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
         blacklistedCol.setCellValueFactory(new PropertyValueFactory<>("blacklisted"));
+        createdAtCol.setCellValueFactory(new PropertyValueFactory<>("createdAt"));
 
         setTable();
-    }
 
-    @FXML
-    void searchBtn(ActionEvent event) {
-        if (ValidationTools.isTextFieldEmptyOrNull(this.searchTf)) {
-            AlertTools.AlertError("Error!", "Search text field is empty!", "Please enter a valid input!");
-            return;
-        }
-
-        String searchText = this.searchTf.getText();
-
-        try {
-            Connection connection = DatabaseTools.getConnection();
-            PreparedStatement statement = connection
-                    .prepareStatement("SELECT * FROM users WHERE role = 'user' AND username LIKE ?");
-            statement.setString(1, "%" + searchText + "%");
-            ResultSet resultSet = statement.executeQuery();
-
-            table.getItems().clear();
-
-            while (resultSet.next()) {
-                table.getItems()
-                        .add(new User(resultSet.getInt("id"), resultSet.getString("username"),
-                                resultSet.getString("password"), resultSet.getString("role"),
-                                resultSet.getString("phone_number"), resultSet.getString("created_at")));
-            }
-
-            DatabaseTools.closeQueryOperationWithPreparedStatement(connection, statement, resultSet);
-
-            UiTools.setTextFieldEmpty(searchTf);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    void backBtn(ActionEvent event) {
-        BackBtnTools.backBtnActionEvent(event);
-    }
-
-    @FXML
-    void addCustomerBtn(ActionEvent event) {
-        SwitchSceneTools.changeSceneActionEvent(event, "../view/customer-add-page.fxml");
-
-        BackBtnTools.addToBackBtnStack("../view/customers-page.fxml");
-    }
-
-    @FXML
-    void detailCustomerBtn(ActionEvent event) {
-        User user = table.getSelectionModel().getSelectedItem();
-
-        if (user != null) {
-            try {
-
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/customer-details-page.fxml"));
-
-                Parent root = loader.load();
-
-                CustomerDetailController controller = loader.getController();
-
-                controller.setUser(user);
-
-                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-                Scene scene = new Scene(root);
-
-                stage.setScene(scene);
-
-                stage.show();
-
-                BackBtnTools.addToBackBtnStack("../view/customers-page.fxml");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            AlertTools.AlertConfirmation("Error", "No User Selected!", "Please select valid user!");
-        }
-    }
-
-    @FXML
-    void editCustomerBtn(ActionEvent event) {
-
-        User user = table.getSelectionModel().getSelectedItem();
-
-        if (user != null) {
-            try {
-
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/customer-edit-page.fxml"));
-
-                Parent root = loader.load();
-
-                CustomerEditController controller = loader.getController();
-
-                controller.setUser(user);
-
-                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-                Scene scene = new Scene(root);
-
-                stage.setScene(scene);
-
-                stage.show();
-
-                BackBtnTools.addToBackBtnStack("../view/customers-page.fxml");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        } else {
-            AlertTools.AlertConfirmation("Error", "No User Selected!", "Please select valid user!");
-        }
-    }
-
-    @FXML
-    void refreshBtn(ActionEvent event) {
-        setTable();
-
-        UiTools.setTextFieldEmpty(searchTf);
+        System.out.println("halo");
     }
 
     private void setTable() {
         table.getItems().clear();
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
         try {
-            Connection connection = DatabaseTools.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM users WHERE role = 'user'");
+            connection = DatabaseTools.getConnection();
+            preparedStatement = connection.prepareStatement("SELECT * FROM customers");
+            resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                table.getItems()
-                        .add(new User(resultSet.getInt("id"), resultSet.getString("username"),
-                                resultSet.getString("password"), resultSet.getString("role"),
-                                resultSet.getString("phone_number"), resultSet.getString("created_at")));
+                table.getItems().add(new Customer(
+                        resultSet.getInt("id"),
+                        resultSet.getString("username"),
+                        resultSet.getString("password"),
+                        resultSet.getString("role"),
+                        resultSet.getString("created_at"),
+                        resultSet.getString("phone_number"),
+                        resultSet.getString("blacklisted")));
             }
 
         } catch (Exception e) {
+            AlertTools.showAlertError("Database connectivity problem!", "Contact support!");
+        } finally {
+            try {
+                if (resultSet != null)
+                    resultSet.close();
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (Exception e) {
+                AlertTools.showAlertError("Database connectivity problem!", "Contact support!");
+            }
+        }
+    }
+
+    @FXML
+    void addOnAction(ActionEvent event) {
+        SwitchSceneTools.changeSceneActionEvent(event, "../view/customer-add-page.fxml");
+
+        BackBtn.addToBackBtnStack("../view/customers-page.fxml");
+    }
+
+    @FXML
+    void backBtn(ActionEvent event) {
+        BackBtn.backBtnActionEvent(event);
+    }
+
+    @FXML
+    void detailOnAction(ActionEvent event) {
+        Customer customer = table.getSelectionModel().getSelectedItem();
+
+        if (customer == null) {
+            AlertTools.showAlertError("Error", "Please select a customer!");
+            return;
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/customer-details-page.fxml"));
+
+            Parent root = loader.load();
+
+            BackBtn.addToBackBtnStack("../view/customers-page.fxml");
+
+            CustomerDetailController controller = loader.getController();
+
+            controller.setCustomer(customer);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    @FXML
+    void editOnAction(ActionEvent event) {
+        Customer customer = table.getSelectionModel().getSelectedItem();
+
+        if (customer == null) {
+            AlertTools.showAlertError("Error", "Please select a customer!");
+            return;
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/customer-edit-page.fxml"));
+
+            Parent root = loader.load();
+
+            BackBtn.addToBackBtnStack("../view/customers-page.fxml");
+
+            CustomerEditController customerEditController = loader.getController();
+
+            customerEditController.setCustomer(customer);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    void refreshOnAction(ActionEvent event) {
+        setTable();
+    }
+
+    @FXML
+    void searchOnAction(ActionEvent event) {
+        if (ValidationTools.isTextFieldEmptyOrNull(searchTf)) {
+            AlertTools.showAlertError("Error", "Search field is empty");
+
+            setTable();
+
+            return;
+        }
+
+        table.getItems().clear();
+
+        Connection connection = DatabaseTools.getConnection();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DatabaseTools.getConnection();
+            preparedStatement = connection.prepareStatement("SELECT * FROM customers WHERE username LIKE ? ");
+            preparedStatement.setString(1, "%" + searchTf.getText() + "%");
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                table.getItems().add(new Customer(
+                        resultSet.getInt("id"),
+                        resultSet.getString("username"),
+                        resultSet.getString("password"),
+                        resultSet.getString("role"),
+                        resultSet.getString("created_at"),
+                        resultSet.getString("phone_number"),
+                        resultSet.getString("blacklisted")));
+            }
+
+        } catch (Exception e) {
+            AlertTools.showAlertError("Database connectivity problem!", "Contact support!");
+        } finally {
+            try {
+                if (resultSet != null)
+                    resultSet.close();
+                if (preparedStatement != null)
+                    preparedStatement.close();
+                if (connection != null)
+                    connection.close();
+            } catch (Exception e) {
+                AlertTools.showAlertError("Database connectivity problem!", "Contact support!");
+            }
+        }
+
+        UiTools.setTextFieldEmpty(searchTf);
     }
 
 }
