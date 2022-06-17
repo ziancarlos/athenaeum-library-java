4f35af6abbb40610b88fb01d5b955ef825a0466a
zian <ziancrlswong@gmail.com>
2022-06-18 00:19:36 +0700

    completed

--- src/controller/BookEditController.java
+++ src/controller/BookEditController.java
@@ -1,8 +1,8 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,7 +11,7 @@ import javafx.scene.control.TextField;
 import model.Book;
 import model.Category;
 import tools.AlertTools;
-import tools.BackBtnTools;
+import tools.BackBtn;
 import tools.DatabaseTools;
 import tools.ValidationTools;
 
@@ -25,52 +25,145 @@ public class BookEditController {
 
     private Book book;
 
+    private int categoryId;
+
+    private void setCategoryCb() {
+        categoryCb.getItems().clear();
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DatabaseTools.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM categories");
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                this.categoryCb.getItems().add(
+                        new Category(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name")));
+            }
+        } catch (Exception e) {
+            AlertTools.showAlertError("Error", e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                AlertTools.showAlertError("Error", e.getMessage());
+            }
+        }
+
+    }
+
     @FXML
     void backBtn(ActionEvent event) {
-        BackBtnTools.backBtnActionEvent(event);
+        BackBtn.backBtnActionEvent(event);
     }
 
     @FXML
     void editBookBtn(ActionEvent event) {
-        if (ValidationTools.isTextFieldEmptyOrNull(bookNameTf)) {
-            AlertTools.AlertError("Error!", "Book Name Text Field Is Empty!", "Please Fill Book Name Text Field!");
+        if (ValidationTools.isTextFieldEmptyOrNull(bookNameTf) || categoryCb.getSelectionModel().isEmpty()) {
+            AlertTools.showAlertError("Error", "Please fill all fields!");
 
             setDefaultTf();
 
             return;
         }
 
-        if (categoryCb.getSelectionModel().getSelectedItem() == null) {
-            AlertTools.AlertError("Error!", "Category Combo Box Is Empty!", "Please Select Category!");
+        if (!ValidationTools.isTextIsValid(3, 46, bookNameTf.getText())) {
+            AlertTools.showAlertError("Text is invalid!", "Please check your text");
 
             setDefaultTf();
 
             return;
         }
 
-        if (bookNameTf.getText().equals(book.getName())
-                && categoryCb.getSelectionModel().getSelectedItem().getId() == book.getCategory().getId()) {
-            AlertTools.AlertError("Error!", "No change has been made!", null);
+        Connection connection = null;
+        PreparedStatement preparedStatementSelect = null;
+        PreparedStatement preparedStatementUpdate = null;
+        ResultSet resultSet = null;
+        int affectedRows;
+
+        try {
+            connection = DatabaseTools.getConnection();
+            connection.setAutoCommit(false);
+
+            preparedStatementSelect = connection
+                    .prepareStatement("SELECT * FROM books WHERE id = ? AND name = ? AND category_id = ? FOR UPDATE");
+            preparedStatementSelect.setInt(1, book.getId());
+            preparedStatementSelect.setString(2, bookNameTf.getText());
+            preparedStatementSelect.setInt(3, categoryCb.getSelectionModel().getSelectedItem().getId());
+
+            resultSet = preparedStatementSelect.executeQuery();
+
+            if (!resultSet.next()) {
+                preparedStatementUpdate = connection
+                        .prepareStatement("UPDATE books SET name = ?, category_id = ? WHERE id = ?");
+                preparedStatementUpdate.setString(1, bookNameTf.getText());
+                preparedStatementUpdate.setInt(2, categoryCb.getSelectionModel().getSelectedItem().getId());
+                preparedStatementUpdate.setInt(3, book.getId());
+
+                affectedRows = preparedStatementUpdate.executeUpdate();
+                if (affectedRows > 0) {
+                    AlertTools.showAlertInformation("Success", "Book updated successfully!");
+                    connection.commit();
+                    BackBtn.backBtnActionEvent(event);
+                } else {
+                    AlertTools.showAlertError("Error", "Book not updated!");
+
+                    setDefaultTf();
+
+                    connection.rollback();
+                }
+            } else {
+                AlertTools.showAlertError("Error", "Book is unchanged!");
+
+                setDefaultTf();
+
+                connection.rollback();
+
+                return;
+            }
+
+        } catch (Exception e) {
+            AlertTools.showAlertError("Error", e.getMessage());
 
             setDefaultTf();
 
-            return;
-        }
+            try {
+                connection.rollback();
+            } catch (Exception ex) {
+                AlertTools.showAlertError("Error", ex.getMessage());
+            }
+        } finally {
+            try {
+                if (preparedStatementUpdate != null) {
+                    preparedStatementUpdate.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
 
-        if (Book.editBook(book, bookNameTf.getText(), categoryCb.getSelectionModel().getSelectedItem())) {
-            AlertTools.AlertInformation("Success!", "Changes Have been made!", null);
-
-            BackBtnTools.backBtnActionEvent(event);
-
-            setDefaultTf();
-
-            return;
-        } else {
-            AlertTools.AlertError("Error!", null, null);
-
-            setDefaultTf();
-
-            return;
+                if (preparedStatementSelect != null) {
+                    preparedStatementSelect.close();
+                }
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (Exception e) {
+                AlertTools.showAlertError("Error", e.getMessage());
+            }
         }
 
     }
@@ -78,29 +171,55 @@ public class BookEditController {
     public void setBook(Book book) {
         this.book = book;
 
+        setDefaultTf();
+
         setCategoryCb();
 
-        setDefaultTf();
+        selectCategoryCb();
+    }
+
+    private void selectCategoryCb() {
+        for (Category category : categoryCb.getItems()) {
+            if (category.getId() == categoryId) {
+                categoryCb.getSelectionModel().select(category);
+                break;
+            }
+        }
     }
 
     private void setDefaultTf() {
-        bookNameTf.setText(book.getName());
-        categoryCb.setValue(book.getCategory());
-    }
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
-    private void setCategoryCb() {
         try {
-            Connection connection = DatabaseTools.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM categories");
+            connection = DatabaseTools.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM books WHERE id = ?");
+            preparedStatement.setInt(1, book.getId());
+            resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()) {
-                categoryCb.getItems().add(new Category(resultSet.getInt("id"), resultSet.getString("name")));
+            if (resultSet.next()) {
+                bookNameTf.setText(resultSet.getString("name"));
+                categoryId = resultSet.getInt("category_id");
+
             }
-
-            DatabaseTools.closeQueryOperation(connection, statement, resultSet);
         } catch (Exception e) {
-            AlertTools.AlertErrorContactSupport();
+            AlertTools.showAlertError("Error", e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                AlertTools.showAlertError("Error", e.getMessage());
+            }
         }
     }
 
